a100d8dc8c5faaffa1b68678080e4d38d7b92c8a
Jake Lever <jake.lever@gmail.com>
2017-09-09 08:30:55 +0000

    Copy files over instead of volumes

--- run_docker_image.sh
+++ run_docker_image.sh
@@ -9,11 +9,16 @@ CONTAINER_NAME=testing
 
 touch install.log test.log final.log
 
-docker run --name ${CONTAINER_NAME} \
-       -v $PWD/install.log:/home/user/install.log \
-       -v $PWD/test.log:/home/user/test.log \
-       -v $PWD/final.log:/home/user/final.log \
-       myimage \
+#docker run --name ${CONTAINER_NAME} \
+#       -v $PWD/install.log:/home/user/install.log \
+#       -v $PWD/test.log:/home/user/test.log \
+#       -v $PWD/final.log:/home/user/final.log \
+#       myimage \
+
+docker run --name ${CONTAINER_NAME} myimage
+docker cp ${CONTAINER_NAME}:/home/user/install.log $PWD/install.log
+docker cp ${CONTAINER_NAME}:/home/user/test.log $PWD/test.log
+docker cp ${CONTAINER_NAME}:/home/user/final.log $PWD/final.log
 
 # Rmove container.
 docker rm ${CONTAINER_NAME}
