a76eca2f6d6f7a3da8e3678d4303814103f33b22
Jake Lever <jake.lever@gmail.com>
2017-09-09 07:54:06 +0000

    Add tar to bare_essentials

--- install_bare_essentials.sh
+++ install_bare_essentials.sh
@@ -2,10 +2,10 @@
 
 if type "yum" > /dev/null; then
 	yum -y update
-	yum -y install gcc curl git ruby vim
+	yum -y install gcc curl git ruby vim tar
 elif type "apt-get" > /dev/null; then
 	apt-get update
-	apt-get install -y build-essential curl git ruby vim
+	apt-get install -y build-essential curl git ruby vim tar
 else
 	echo "No yum or apt-get to install bare essentials."
 	exit 1
