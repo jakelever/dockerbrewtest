8696824e477b578ae84ffdb6c939dc68914fc3f1
Jake Lever <jlever@bcgsc.ca>
2019-04-24 15:27:57 -0700

    Update to latest Linuxbrew install method

--- test_linuxbrew.sh
+++ test_linuxbrew.sh
@@ -2,7 +2,8 @@
 
 set -x
 
-git clone https://github.com/Linuxbrew/brew.git ~/.linuxbrew
+#git clone https://github.com/Linuxbrew/brew.git ~/.linuxbrew
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
 
 export PATH="$HOME/.linuxbrew/bin:$PATH"
 export MANPATH="$(brew --prefix)/share/man:$MANPATH"
@@ -11,7 +12,10 @@ export INFOPATH="$(brew --prefix)/share/info:$INFOPATH"
 hash -r
 sleep 1
 
-brew install gcc &> install.log
-brew test gcc &> test.log
-echo $? > final.log
+brew install gcc #&> install.log
+brew test gcc #&> test.log
+#echo $? > final.log
+
+brew install git
+brew test git
 
