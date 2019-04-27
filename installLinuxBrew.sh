04f6cbf68a7057854bb602482d79d947fa889238
Jake Lever <jlever@bcgsc.ca>
2019-04-27 16:46:01 -0700

    Add in special case for CentOS 6

--- installLinuxBrew.sh
+++ installLinuxBrew.sh
@@ -8,5 +8,16 @@ mkdir ~/.linuxbrew/bin
 ln -s ../Homebrew/bin/brew ~/.linuxbrew/bin
 eval $(~/.linuxbrew/bin/brew shellenv)
 
+platform=$(python -mplatform)
+if [[ $platform == *"centos.6"* ]]; then
+	HOMEBREW_NO_AUTO_UPDATE=1 HOMEBREW_BUILD_FROM_SOURCE=1 brew install gcc --without-glibc
+	HOMEBREW_NO_AUTO_UPDATE=1 brew install glibc
+# If encountering locale errors in postinstall of glibc, run 
+# HOMEBREW_NO_AUTO_UPDATE=1 LC_CTYPE=en_GB.UTF-8 brew postinstall glibc
+# see issue - https://github.com/Linuxbrew/legacy-linuxbrew/issues/929
+	HOMEBREW_NO_AUTO_UPDATE=1 brew remove gcc
+	HOMEBREW_NO_AUTO_UPDATE=1 brew install gcc
+fi
+
 brew install hello
 
