2ce855fe15bb23a73f11a83e96d2e4520cb9bf75
Jake Lever <jlever@bcgsc.ca>
2019-04-27 16:20:39 -0700

    Add tar to yum not apt-get

--- setupBasicsAndUser.sh
+++ setupBasicsAndUser.sh
@@ -5,14 +5,19 @@ useradd -m -s /bin/bash gromit
 
 if type "yum" > /dev/null; then
 	yum -y update
-	#yum -y install gcc curl git ruby vim tar
 	yum groupinstall -y 'Development Tools'
 	yum install -y curl file git
+
+	# Extra
+	yum install -y tar
 elif type "apt-get" > /dev/null; then
 	apt-get update
-	#apt-get install -y build-essential curl git ruby vim tar
-	apt-get install -y build-essential curl file git gawk tar
+	apt-get install -y build-essential curl file git
+	
+	# Extra
+	apt-get install -y gawk
 else
 	echo "No yum or apt-get to install bare essentials."
 	exit 1
 fi
+
